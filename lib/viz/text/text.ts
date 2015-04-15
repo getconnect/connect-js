51c1d24be5f8baa4ce863a009163e9e814c3aed4
teamconnect <team@getconnect.io>
2015-04-15 15:04:01 +1000

    make font selection more specific

--- lib/viz/text/text.ts
+++ lib/viz/text/text.ts
@@ -111,7 +111,8 @@ class Text implements Common.Visualization {
 
         container.className = 'connect-viz connect-text';
         label.className = 'connect-viz-title';
-        valueElement.className = 'connect-viz-result connect-text-value';
+        valueElement.className = 'connect-viz-result';
+        valueTextElement.className = 'connect-text-value'
         valueIncreaseIconElement.className = 'connect-text-icon connect-text-icon-increase ion-arrow-up-b';
         valueDecreaseIconElement.className = 'connect-text-icon connect-text-icon-decrease ion-arrow-down-b';
 
